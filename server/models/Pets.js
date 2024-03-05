c8e97dd2bd505a56d8883ed48e52948694bad3f1
dakotawentz <dswentz@gmail.com>
2024-03-04 18:47:46 -0800

    Added PetSchema to backend models

---
+++ server/models/Pets.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const PetSchema = new Schema(
+    {
+        name: {
+        type: String,
+        required: true,
+        },
+        pic: {
+        type: String,
+        required: true,
+        },
+        species: {
+        type: String,
+        required: true,
+        },
+        color: {
+        type: String,
+        required: true,
+        },
+        age: {
+        type: Number,
+        required: true,
+        },
+        gender: {
+        type: String,
+        required: true,
+        },
+    }
+);
+
+
+const Pets = model('Pets', PetsSchema);
+
+module.exports = Pets;
