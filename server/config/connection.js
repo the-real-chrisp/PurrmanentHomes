f3c724e7d607917d6878d8f9c908a4175927b599
Chris Purcell <chrisp0000@gmail.com>
2024-03-04 21:10:01 -0800

    Removed await

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 try{
-   await mongoose.connect('mongodb://127.0.0.1:27017/purrmanent-homes');
+    mongoose.connect('mongodb://127.0.0.1:27017/purrmanent-homes');
 } catch (err) {
     console.log(err)
 }
