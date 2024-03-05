58a5bd33ec69f0104e4ab12fce411eae82855312
dakotawentz <dswentz@gmail.com>
2024-03-04 19:18:16 -0800

    added getPet and getPets queries. added createPet mutation as well

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Pet } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth')
 const resolvers = {
   Query: {
@@ -16,37 +16,62 @@ const resolvers = {
       return users
     },
     findUser: async (parent, args, context) => {
-        const userData = await User.findOne({_id: args.id})
-        return userData;
+      const userData = await User.findOne({ _id: args.id })
+      return userData;
+    },
+    getPet: async (parent, args, context) => {
+      const pet = await Pet.findOne({ _id: args.id })
+      return pet;
+    },
+    getPets: async (parent, args, context) => {
+      const pets = await Pet.find()
+      return pets
+    },
+    Mutation: {
+      login: async (parent, args) => {
+        const user = await User.findOne({ email: args.email });
+        if (!user) {
+          throw AuthenticationError;
+        }
+
+        const correctPw = await user.isCorrectPassword(args.password);
+
+        if (!correctPw) {
+          throw AuthenticationError;
+        }
+        const token = signToken(user);
+        return { user, token };
+      },
+
+      addUser: async (parent, args) => {
+        const user = await User.create({ username: args.username, email: args.email, password: args.password });
+        console.log(user);
+        if (!user) {
+          throw AuthenticationError;
+        }
+        const token = signToken(user);
+        return { user, token };
+      },
+
+      createPet: async (_, { name, pic, species, color, age, gender }) => {
+        try {
+          const newPet = new Pet({
+            name,
+            pic,
+            species,
+            color,
+            age,
+            gender,
+          });
+          const savedPet = await newPet.save();
+          return savedPet;
+        } catch (error) {
+          throw new Error('Failed to create pet');
+        }
+
+      },
     }
-  },
-  Mutation: {
-    login: async (parent, args) => {
-      const user = await User.findOne({ email: args.email });
-      if (!user) {
-        throw AuthenticationError;
-      }
-
-      const correctPw = await user.isCorrectPassword(args.password);
-
-      if (!correctPw) {
-        throw AuthenticationError;
-      }
-      const token = signToken(user);
-      return { user, token };
-    },
-
-    addUser: async (parent, args) => {
-      const user = await User.create({username: args.username, email: args.email, password: args.password});
-      console.log(user);
-      if (!user) {
-        throw AuthenticationError;
-      }
-      const token = signToken(user);
-      return { user, token };
-    },
-  
-  },
+  }
 };
 
 module.exports = resolvers;
